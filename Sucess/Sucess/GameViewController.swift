64ae27740ba42f758e27f9be8ed4b65df1e612f1
Dano_MAC <leonid_gomez@cev.com>
2024-10-23 10:22:10 +0100

    first screen and connections

---
+++ Sucess/Sucess/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIView.swift
+//  Sucess
+//
+//  Created by user234564 on 10/23/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+

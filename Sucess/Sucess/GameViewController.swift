3b4c3d4d1b698a1405bf61d97ee25d8f0a0329a1
Dano_MAC <leonid_gomez@cev.com>
2024-10-24 11:58:31 +0100

    Screen 1 and connections

--- Sucess/Sucess/GameViewController.swift
+++ Sucess/Sucess/GameViewController.swift
@@ -5,12 +5,10 @@
 //  Created by user234564 on 10/23/24.
 //
 
-import SwiftUI
+import UIKit
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+class GameViewController: UIViewController {
+
+
+
 }
-
-

a3abad2fec17349b44dd3430796ecd68f8c79232
Dano_MAC <leonid_gomez@cev.com>
2024-10-25 08:31:46 +0100

    Initial Commit

---
+++ SucessAppTests/SucessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SucessAppTests.swift
+//  SucessAppTests
+//
+//  Created by user234564 on 10/25/24.
+//
+
+import Testing
+@testable import SucessApp
+
+struct SucessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
